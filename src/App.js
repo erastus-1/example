95cd60bf6366228eec8fb999d42f9feeff6ec810
erastus <erastuskariuki15@gmail.com>
2021-03-23 19:58:11 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,53 +1,19 @@
 import React, { Component } from "react";
-import Home from "./components/Home";
+//import Home from "./components/Home";
 import Index from "./components/Index";
 import Landing from "./components/Landing";
 import Footer from "./components/Footer";
 import Products from "./components/Products";
-import { BrowserRouter as Router, Route } from "react-router-dom"
-
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <Router>
-        <NavBar />
-
-        <Route
-          path="/"
-          exact
-          render={() => 
-          <Home />}
-        />
-        <Route
-          path="/"
-          exact
-          render={() => 
-          <Index />}
-        />
-
-        <Route
-          path="/"
-          exact
-          render={() => 
-          <Products />}
-        />
-        <Route
-          path="/"
-          exact
-          render={() => 
-          <Landing />}
-        />
-
-        <Route
-          path="/"
-          exact
-          render={() => 
-          <Footer />}
-        />
-        </Router>
-    </div>
+      <div className="App">
+        <Index />
+        <Products />
+        <Landing />
+        <Footer />
+      </div>
     );
   }
 }
